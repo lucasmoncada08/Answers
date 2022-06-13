6aac98414bc3a3e9cc22731f159f15ee9466185f
Lucas Moncada <lucas.moncada08@gmail.com>
2022-06-13 09:29:50 -0400

    New components with heading beginnings

--- src/App.js
+++ src/App.js
@@ -2,16 +2,16 @@ import React, { useState} from 'react'
 
 import Navbar from './components/Navbar'
 import Footer from './components/Footer'
-import Routes from './components/Routes'
+import RRoutes from './components/RRoutes'
 
 const App = () => {
   const [darkTheme, setDarkTheme] = useState(false)
 
   return (
     <div className={darkTheme ? 'dark': ''}>
-      <div className='text-2xl bg-gray-300 dark:bg-gray-700 dark:text-gray-200 min-h-screen'>
-        <Navbar></Navbar>
-        <Routes></Routes>
+      <div className='bg-gray-100 dark:bg-gray-700 dark:text-gray-200 min-h-screen'>
+        <Navbar darkTheme={darkTheme} setDarkTheme={setDarkTheme}></Navbar>
+        <RRoutes></RRoutes>
         <Footer></Footer>
       </div>
     </div>
--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { BrowserRouter as Router } from 'react-router-dom'
 
 import App from './App'
 import './index.css'
@@ -9,5 +10,7 @@ const container = document.getElementById('root')
 const root = ReactDOM.createRoot(container)
 
 root.render(
-    <App />
+    <Router>
+        <App />
+    </Router>
 )
