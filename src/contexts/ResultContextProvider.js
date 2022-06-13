b804940c4bdc82e70d85560a9a523c5d06cec2ff
lucasmoncada08 <lucas.moncada08@gmail.com>
2022-06-13 11:24:26 -0400

    Added image and news capabilities

---
+++ src/contexts/ResultContextProvider.js
@@ -0,0 +1,43 @@
+import { Result } from 'postcss'
+import React, { createContext, useContext, useState } from 'react'
+
+const ResultContext = createContext()
+const baseUrl = 'https://google-search3.p.rapidapi.com/api/v1'
+
+export const ResultContextProvider = ({ children }) => {
+    const [results, setResults] = useState([])
+    const [isLoading, setIsLoading] = useState(false)
+    const [searchTerm, setSearchTerm] = useState('answers')
+
+    const getResults = async (type) => {
+        setIsLoading(true)
+        const response = await fetch(`${baseUrl}${type}`, {
+            method: 'GET',
+            headers: {
+                'X-User-Agent': 'desktop',
+                'X-Proxy-Location': 'CA',
+                'X-RapidAPI-Key': '99a71675b3msh90b4f5556771a72p121b18jsn0050dbec2633',
+                'X-RapidAPI-Host': 'google-search3.p.rapidapi.com'
+            }
+        })
+
+        const data = await response.json()
+
+        if (type.includes('/news')) 
+            setResults(data.entries)
+        else if (type.includes('/images'))
+            setResults(data.image_results)
+        else
+            setResults(data.results)
+
+        setIsLoading(false)
+    }
+
+    return (
+        <ResultContext.Provider value={{ getResults, results, searchTerm, setSearchTerm, isLoading }}>
+            {children}
+        </ResultContext.Provider>
+    )
+}
+
+export const useResultContext = () => useContext(ResultContext)
--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client'
 import { BrowserRouter as Router } from 'react-router-dom'
 
 import App from './App'
+import { ResultContextProvider } from './contexts/ResultContextProvider'
 import './index.css'
 
 const container = document.getElementById('root')
@@ -10,7 +11,9 @@ const container = document.getElementById('root')
 const root = ReactDOM.createRoot(container)
 
 root.render(
-    <Router>
-        <App />
-    </Router>
+    <ResultContextProvider>
+        <Router>
+            <App />
+        </Router>
+    </ResultContextProvider>
 )
